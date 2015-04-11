f6c85cb5dffc544796c74e34387a232e6c018564
Evgeny Cherkashin <eugene@irnok.net>
2015-04-11 09:25:35 +0800

    Added some simulation.

--- model.pas
+++ model.pas
@@ -9,10 +9,10 @@ uses
   StdCtrls;
 
 const
-     defVx=8e3; // 8*10^3
+     defVx=8e3; // 8*10^3 m/s
      defVy=0.0;
      defH =400.0; // In km
-     RE=6371;
+     RE=6371000;
      SS=20;
 
 type
@@ -36,9 +36,14 @@ type
     procedure FormCreate(Sender: TObject);
     procedure TimerTimer(Sender: TObject);
   private
+    { private declarations }
     scale:Real;
     bmp:TBitmap;
-    { private declarations }
+    Sx,Sy:Integer;
+    X,Y:Real;
+    Vxs,Vys:Real;
+    Sim:Boolean;
+    procedure PutPoint(pX,pY:Real; col:TColor);
   public
     { public declarations }
   end;
@@ -74,30 +79,57 @@ begin
   Cx:=C.Width >> 1;
   Cy:=C.Height >> 1;
   C.Ellipse(Cx-R,Cy-R,Cx+R,Cy+R);
+  Sx:=Screen.Width >> 1;
+  Sy:=Screen.Height >> 1;
+  Sim:=False;
 end;
 
 procedure TMainForm.TimerTimer(Sender: TObject);
+var
+  C:TCanvas;
 begin
-
+  X:=X+Vxs;
+  Y:=Y+Vys;
+  PutPoint(X,Y,clBlack);
+  FigurePaint(Sender);
 end;
-
 
 procedure TMainForm.StartClick(Sender: TObject);
 begin
-   FigurePaint(Sender);
+  Timer.Enabled:=not Timer.Enabled;
+  Sim:=Timer.Enabled;
+  If Sim Then
+    begin
+        Y:=RE+StrToFloat(H.Text)*1000;
+        X:=0.0;
+        Vxs:=StrToFloat(Vx.Text);
+        Vys:=StrToFloat(Vy.Text);
+        PutPoint(X,Y, clBlue);
+        FigurePaint(Sender);
+        Start.Caption:='Stop'
+    end
+  Else
+    Start.Caption:='Start';
+end;
+
+procedure TMainForm.PutPoint(pX,pY:Real; col:TColor);
+var
+  xi,yi:integer;
+begin
+  xi:=round(Sx+pX*scale);      // As the coordinate system is inverted
+  yi:=round(Sy-pY*scale);
+  bmp.Canvas.Pixels[xi,yi]:=col;
+  bmp.Canvas.Rectangle(xi-2,yi-2,xi+2,yi+2);
 end;
 
 procedure TMainForm.FigurePaint(Sender: TObject);
 var
   cx,cy:integer;
-  sx,sy:integer;
   C:TCanvas;
 begin
   C:=Figure.Canvas;
   Cx:=C.Width >> 1;
   Cy:=C.Height >> 1;
-  Sx:=Screen.Width >> 1;
-  Sy:=Screen.Height >> 1;
   C.Draw(Cx-Sx,Cy-Sy,bmp);  //TCanvas
 end;
 
